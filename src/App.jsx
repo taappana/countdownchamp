6c6605b3bbe3a8560cdeab6e1c02c8e4de68e156
taappana <herranen.jani@gmail.com>
2017-05-24 19:17:06 +0300

    Chancing data with the input field now works

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,24 @@ import React, { Component } from 'react';
 import './App.css'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      deadline: 'December 25, 2017',
+      newDeadline: ''
+    }
+  }
+
+  changeDeadline() {
+    this.setState({deadline: this.state.newDeadline});
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-title">Countdown to December 25, 2017</div>
+        <div className="App-title">
+          Countdown to {this.state.deadline}
+        </div>
         <div>
           <div className="Clock-days">14 days</div>
           <div className="Clock-hours">30 hours</div>
@@ -13,8 +27,13 @@ class App extends Component {
           <div className="Clock-seconds">20 seconds</div>
         </div>
         <div>
-          <input placeholder='new date'/>
-          <button>Submit</button>
+          <input
+            placeholder='new date'
+            onChange={event => this.setState({newDeadline: event.target.value})}
+          />
+          <button onClick={() => this.changeDeadline()}>
+            Submit
+          </button>
         </div>
 
       </div>
